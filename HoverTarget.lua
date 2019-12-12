37c3c3a8da61eaff4388669277d2b13f66f7f296
Kaan Demiroz <kaan@fun-gi.com>
2019-12-11 23:28:18 -0800

    Prevent taint by adding combat checks and converting to local functions

--- HoverTarget.lua
+++ HoverTarget.lua
@@ -1,9 +1,33 @@
 -- Copyright 2019, Shadock
+local addonName, addon = ...
+local HoverTarget = addon
+
+local HoverTargetFrame = CreateFrame("Button", "HoverTargetFrame", GameTooltip, "TargetFrameTemplate")
+
+local isInit = false
+
 local FRAME_OFFSET_X = 40
 local FRAME_OFFSET_Y = 40
 local UNIT_MOUSEOVER = "mouseover"
 
-function HoverTargetFrame_OnLoad (self)
+function HoverTargetFrame:Initialize ()
+	self:SetScript("OnHide", self.OnHide)
+	self:SetScript("OnEvent", self.OnEvent)
+	self:SetScript("OnUpdate", self.OnUpdate)
+	self:OnLoad()
+end
+
+function HoverTargetFrame:InitializePoint()
+	local anchorRight = GameTooltip:GetRight() + FRAME_OFFSET_X
+	local anchorBottom = GameTooltip:GetTop() + FRAME_OFFSET_Y
+
+	self:SetParent(GameTooltip)
+	self:ClearAllPoints()
+	self:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMLEFT", anchorRight, anchorBottom)
+	-- self:SetPoint("BOTTOMRIGHT", GameTooltip, "TOPRIGHT", FRAME_OFFSET_X, FRAME_OFFSET_Y)
+end
+
+function HoverTargetFrame:OnLoad ()
 	self:SetWidth(TargetFrame:GetWidth())
 	self:SetHeight(TargetFrame:GetHeight())
 
@@ -21,16 +45,18 @@ function HoverTargetFrame_OnLoad (self)
 	self.showCastbar = TargetFrame.showCastbar
 
     local unit = UNIT_MOUSEOVER
-	TargetFrame_OnLoad(self, unit);
-	TargetFrame_CreateTargetofTarget(self, unit.."target");
+	local totUnit = unit.."target"
+	TargetFrame_OnLoad(self, unit)
+	TargetFrame_CreateTargetofTarget (self, totUnit)
+	UnitFrame_SetUnit(self, unit,  self.healthbar,  self.manabar)
+	UnitFrame_SetUnit (self.totFrame, totUnit, self.totFrame.healthbar, self.totFrame.manabar)
 
-	self.totFrame:SetScript("OnUpdate", HoverTargetofTarget_Update)
-	self.totFrame:SetScript("OnHide", HoverTargetofTarget_OnHide)
+	self.totFrame:SetScript("OnUpdate", TargetofTarget_Update)
 
 	self:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
 end
 
-function HoverTargetFrame_MouseoverUnit (self)
+function HoverTargetFrame:MouseoverUnit ()
     local unit = UNIT_MOUSEOVER
 	if not UnitExists(unit) then
 		self.healthbar.lockValues = true
@@ -40,72 +66,136 @@ function HoverTargetFrame_MouseoverUnit (self)
 			self.totFrame.manabar.lockValues = true
 		end
 	else
-		HoverTargetFrame_SetUnit(self, unit)
+		self:SetUnit(unit)
 	end
 end
 
-function HoverTargetFrame_SetUnit (self, unit)
+function HoverTargetFrame:SetUnit (unit)
 	if not UnitExists(unit) then
 		return
 	end
 
-	HoverTargetFrame_InsertToTooltip(self, GameTooltip)
+	if not self.isInit and not InCombatLockdown() then
+		self:InitializePoint()
+		self.isInit = true
+	end
 
 	self.healthbar.lockValues = false
 	self.manabar.lockValues = false
-	UnitFrame_SetUnit(self, unit,  self.healthbar,  self.manabar)
 
-	local totUnit = unit.."target"
 	if (self.totFrame) then
 		self.totFrame.healthbar.lockValues = false
 		self.totFrame.manabar.lockValues = false
-		UnitFrame_SetUnit (self.totFrame, totUnit, self.totFrame.healthbar, self.totFrame.manabar)
 	end
 
-	TargetFrame_Update(self)
+	self:Target_Update()
 end
 
-function HoverTargetFrame_InsertToTooltip (self, tooltipFrame)
-	local anchorRight = GameTooltip:GetRight() + FRAME_OFFSET_X
-	local anchorBottom = GameTooltip:GetTop() + FRAME_OFFSET_Y
+-- Modified code from Blizzard's TargetFrame.lua to control in-combat execution
+function HoverTargetFrame:Target_Update ()
+	-- This check is here so the frame will hide when the target goes away
+	-- even if some of the functions below are hooked by addons.
+	if ( not UnitExists(self.unit) and not ShowBossFrameWhenUninteractable(self.unit) ) then
+		if not InCombatLockdown() then
+			self:Hide();
+		end
+	else
+		if not InCombatLockdown() then
+			self:Show();
+		end
 
-	self:SetParent(tooltipFrame)
-	self:ClearAllPoints()
-	self:SetPoint("BOTTOMRIGHT", UIParent, "BOTTOMLEFT", anchorRight, anchorBottom)
+		-- Moved here to avoid taint from functions below
+		if ( self.totFrame ) then
+			self:TargetofTarget_Update(self.totFrame);
+		end
 
-	if ( not tooltipFrame.insertedFrames ) then
-		tooltipFrame.insertedFrames = { };
+		UnitFrame_Update(self);
+		if ( self.showLevel ) then
+			TargetFrame_CheckLevel(self);
+		end
+		TargetFrame_CheckFaction(self);
+		if ( self.showClassification ) then
+			TargetFrame_CheckClassification(self);
+		end
+		TargetFrame_CheckDead(self);
+		TargetFrame_CheckDishonorableKill(self);
+		if ( self.showLeader ) then
+			if ( UnitLeadsAnyGroup(self.unit) ) then
+				self.leaderIcon:SetTexture("Interface\\GroupFrame\\UI-Group-LeaderIcon");
+				self.leaderIcon:SetTexCoord(0, 1, 0, 1);
+				self.leaderIcon:Show();
+			else
+				self.leaderIcon:Hide();
+			end
+		end
+		TargetFrame_UpdateAuras(self);
+		if ( self.portrait ) then
+			self.portrait:SetAlpha(1.0);
+		end
+		TargetFrame_CheckBattlePet(self);
+		if ( self.petBattleIcon ) then
+			self.petBattleIcon:SetAlpha(1.0);
+		end
 	end
-	local frameWidth = self:GetWidth();
-	if (tooltipFrame:GetMinimumWidth() < frameWidth) then
-		tooltipFrame:SetMinimumWidth(frameWidth);
-	end
-	tinsert(tooltipFrame.insertedFrames, self);
 end
 
-function HoverTargetFrame_OnUpdate (self, elapsed)
+-- Modified code from Blizzard's TargetFrame.lua to control in-combat execution
+function HoverTargetFrame:TargetofTarget_Update (self, elapsed)
+	local show;
+	local parent = self:GetParent();
+	if ( SHOW_TARGET_OF_TARGET == "1" and UnitExists(parent.unit) and UnitExists(self.unit) and ( not UnitIsUnit(PlayerFrame.unit, parent.unit) ) and ( UnitHealth(parent.unit) > 0 ) ) then
+		if ( not self:IsShown() ) then
+			if not InCombatLockdown() then
+				self:Show();
+			end
+			if ( parent.spellbar ) then
+				parent.haveToT = true;
+				Target_Spellbar_AdjustPosition(parent.spellbar);
+			end
+		end
+		UnitFrame_Update(self);
+		TargetofTarget_CheckDead(self);
+		TargetofTargetHealthCheck(self);
+		RefreshDebuffs(self, self.unit, nil, nil, true);
+	else
+		if ( self:IsShown() ) then
+			if not InCombatLockdown() then
+				self:Hide();
+			end
+			if ( parent.spellbar ) then
+				parent.haveToT = nil;
+				Target_Spellbar_AdjustPosition(parent.spellbar);
+			end
+		end
+	end
+end
+
+function HoverTargetFrame:OnHide ()
+	if InCombatLockdown() then
+		return
+	end
+	self:SetParent(nil)
+	self:Hide()
+	self.isInit = false
+end
+
+function HoverTargetFrame:OnUpdate (elapsed)
 	local unit = UNIT_MOUSEOVER
-	if UnitExists(unit) then
+	if UnitExists(unit) and not InCombatLockdown() then
 		TargetFrame_OnUpdate(self, elapsed)
 	end
 end
 
-function HoverTargetFrame_OnEvent (self, event, ...)
+function HoverTargetFrame:OnEvent (event, ...)
 	if ( event == "UPDATE_MOUSEOVER_UNIT" ) then
-		HoverTargetFrame_MouseoverUnit(self)
+		self:MouseoverUnit()
 	end
 end
 
-function HoverTargetFrame_OnHide (self)
-	-- Do nothing
-end
-
-function HoverTargetofTarget_Update (self)
-	if UnitExists(self.unit) then
-		TargetofTarget_Update(self)
+hooksecurefunc("GameTooltip_ClearInsertedFrames", function (self)
+	if not InCombatLockdown() then
+		HoverTargetFrame:Hide()
 	end
-end
+end)
 
-function HoverTargetofTarget_OnHide (self)
-	-- Do nothing
-end
+HoverTargetFrame:Initialize()
